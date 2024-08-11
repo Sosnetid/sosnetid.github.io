b612262e3ce7096f1794e7097ae4aafc97c27ad9
Sosnetid <flamixs.info@gmail.com>
2024-08-11 18:51:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,9 @@
-$(window).load(function() {
+$(window).on('load', function() {
     $("#preloader").fadeOut("slow");
 });
 
 $(document).ready(function(){
-      
-});
-    });
-
-    wow = new WOW({
-        mobile:       false,       // default
-      }
-    )
-    wow.init();
-
-     $('#top-nav').onePageNav({
-        currentClass: 'current',
-        changeHash: true,
-        scrollSpeed: 1200
-    });
-
-     
-    //animated header class
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".navbar-default").addClass("animated");
-        } else {
-            $(".navbar-default").removeClass('animated');
-        }
-    });
-
-    $('#countdown_dashboard').countDown({
-        targetDate: {
-            'day':      11,
-            'month':    3,
-            'year':     2017,
-            'hour':     00,
-            'min':      00,
-            'sec':      01,
-        },
-        omitWeeks: true
-    });
-
+    // Inisialisasi Owl Carousel
     $('.init-slider').owlCarousel({
         items:1,
         merge:true,
@@ -49,17 +12,42 @@ $(document).ready(function(){
         smartSpeed: 600
     });
 
-    /*$('input, textarea').data('holder', $('input, textarea').attr('placeholder'));
-
-    $('input, textarea').focusin(function () {
-        $(this).attr('placeholder', '');
+    // Inisialisasi WOW.js
+    var wow = new WOW({
+        mobile: false
     });
-    $('input, textarea').focusout(function () {
-        $(this).attr('placeholder', $(this).data('holder'));
-    });*/
+    wow.init();
 
+    // Navigasi halaman satu (One Page Navigation)
+    $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
 
-    //contact form validation
+    // Tambahkan kelas 'animated' pada header saat scroll
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar-default").addClass("animated");
+        } else {
+            $(".navbar-default").removeClass('animated');
+        }
+    });
+
+    // Inisialisasi countdown
+    $('#countdown_dashboard').countDown({
+        targetDate: {
+            'day': 11,
+            'month': 3,
+            'year': 2017,
+            'hour': 00,
+            'min': 00,
+            'sec': 01
+        },
+        omitWeeks: true
+    });
+
+    // Validasi form kontak
     $("#contact-form").validate({
         rules: {
             name: {
@@ -77,30 +65,30 @@ $(document).ready(function(){
         },
         messages: {
             name: {
-                required: "Please enter Your Name",
-                minlength: "Your name must consist of at least 2 characters"
+                required: "Harap masukkan Nama Anda",
+                minlength: "Nama Anda harus terdiri dari setidaknya 2 karakter"
             },
             message: {
-                required: "Please Write Something",
-                minlength: "Your message must consist of at least 2 characters"
+                required: "Harap tulis sesuatu",
+                minlength: "Pesan Anda harus terdiri dari setidaknya 2 karakter"
             },
-            email: "Please enter a valid email address"
+            email: "Harap masukkan alamat email yang valid"
         },
         submitHandler: function(form) {
             $(form).ajaxSubmit({
-                type:"POST",
+                type: "POST",
                 data: $(form).serialize(),
-                url:"mail.php",
+                url: "mail.php",
                 success: function() {
                     $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                    $('#contact-form').fadeTo("slow", 0.15, function() {
                         $(this).find(':input').attr('disabled', 'disabled');
                         $(this).find('label').css('cursor','default');
                         $('#success').fadeIn();
                     });
                 },
                 error: function() {
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                    $('#contact-form').fadeTo("slow", 0.15, function() {
                         $('#error').fadeIn();
                     });
                 }
@@ -108,35 +96,21 @@ $(document).ready(function(){
         }
     });
 
+    // Inisialisasi Google Map
+    google.maps.event.addDomListener(window, 'load', init);
 });
 
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 16,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka ,
+        center: new google.maps.LatLng(23.751945, 90.384590),
         scrollwheel: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
         styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(23.751945, 90.384590),
         map: map,
