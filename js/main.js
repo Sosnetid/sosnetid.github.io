b8d8e10d7641c5c29e97366b54401955bfa8c01a
Sosnetid <flamixs.info@gmail.com>
2024-08-11 18:45:25 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,7 @@ $(window).load(function() {
 });
 
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        items:1,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        animateOut: 'fadeOut'
-    });
+      
 });
     });
 
