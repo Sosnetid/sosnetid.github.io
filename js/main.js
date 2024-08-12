0ee54c9c92ac0e7ab996bbf57a7569752726b662
Sosnetid <flamixs.info@gmail.com>
2024-08-12 10:45:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,45 @@
-$(window).load(function() {
+$(window).on('load', function() {
     $("#preloader").fadeOut("slow");
 });
 
 $(document).ready(function(){
+    new WOW({ mobile: false }).init();
 
-    wow = new WOW({
-        mobile:       false,       // default
-      }
-    )
-    wow.init();
-
-     $('#top-nav').onePageNav({
+    $('#top-nav').onePageNav({
         currentClass: 'current',
         changeHash: true,
         scrollSpeed: 1200
     });
 
-     
-    //animated header class
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".navbar-default").addClass("animated");
-        } else {
-            $(".navbar-default").removeClass('animated');
-        }
+        $(".navbar-default").toggleClass("animated", $(window).scrollTop() > 100);
     });
 
     $('#countdown_dashboard').countDown({
         targetDate: {
-            'day':      11,
-            'month':    3,
-            'year':     2017,
-            'hour':     00,
-            'min':      00,
-            'sec':      01,
+            'day': 11,
+            'month': 3,
+            'year': 2017,
+            'hour': 00,
+            'min': 00,
+            'sec': 01,
         },
         omitWeeks: true
     });
 
     $('.init-slider').owlCarousel({
-        items:1,
-        merge:true,
-        loop:true,
-        video:true,
+        items: 1,
+        merge: true,
+        loop: true,
+        video: true,
         smartSpeed: 600
     });
 
-    /*$('input, textarea').data('holder', $('input, textarea').attr('placeholder'));
-
-    $('input, textarea').focusin(function () {
-        $(this).attr('placeholder', '');
-    });
-    $('input, textarea').focusout(function () {
-        $(this).attr('placeholder', $(this).data('holder'));
-    });*/
-
-
-    //contact form validation
     $("#contact-form").validate({
         rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            message: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
+            name: { required: true, minlength: 2 },
+            message: { required: true, minlength: 2 },
+            email: { required: true, email: true }
         },
         messages: {
             name: {
@@ -85,19 +54,19 @@ $(document).ready(function(){
         },
         submitHandler: function(form) {
             $(form).ajaxSubmit({
-                type:"POST",
+                type: "POST",
                 data: $(form).serialize(),
-                url:"mail.php",
+                url: "mail.php",
                 success: function() {
                     $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                    $('#contact-form').fadeTo("slow", 0.15, function() {
                         $(this).find(':input').attr('disabled', 'disabled');
                         $(this).find('label').css('cursor','default');
                         $('#success').fadeIn();
                     });
                 },
                 error: function() {
-                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                    $('#contact-form').fadeTo("slow", 0.15, function() {
                         $('#error').fadeIn();
                     });
                 }
@@ -105,76 +74,34 @@ $(document).ready(function(){
         }
     });
 
+    $("#about-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: true,
+        navText: ["&#10094;","&#10095;"]
+    });
 });
 
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+google.maps.event.addDomListener(window, 'load', function() {
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 16,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka ,
+        center: new google.maps.LatLng(23.751945, 90.384590), // Dhaka
         scrollwheel: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
         styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
     };
-let currentSlide = 0;
 
-function showSlide(index) {
-  const slides = document.querySelectorAll('.slide');
-  if (index >= slides.length) currentSlide = 0;
-  if (index < 0) currentSlide = slides.length - 1;
-
-  slides.forEach((slide, i) => {
-    slide.style.display = i === currentSlide ? 'block' : 'none';
-  });
-}
-
-function moveSlide(step) {
-  currentSlide += step;
-  showSlide(currentSlide);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  showSlide(currentSlide);
-    setInterval(() => {
-  moveSlide(1);
-}, 3000); // Slide akan bergerak setiap 3 detik
-    $(document).ready(function(){
-  $("#about-carousel").owlCarousel({
-      items: 1,
-      loop: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      nav: true,
-      dots: true,
-      navText: ["&#10094;","&#10095;"]
-  });
-});
-
-
-});
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(23.751945, 90.384590),
         map: map,
         icon: 'img/map.png',
         title: 'Twing!'
     });
-}
+});
